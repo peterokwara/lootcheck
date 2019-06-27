5affc5023f80e068a9728f7ba2e809ab4ad5e3c0
peterokwara <peter.okwara@outlook.com>
2019-06-27 15:47:38 +0300

    Add code for depositing balance to reducer

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -4,7 +4,8 @@ const balance = (state = 0, action) => {
   switch (action.type) {
     case constants.SET_BALANCE:
       return action.balance;
-
+    case constants.DEPOSIT:
+      return state + action.deposit;
     default:
       return state;
   }
