e8c6725d8c927e9f99822383c3f3489a182e3dfa
peterokwara <peter.okwara@outlook.com>
2019-06-27 18:01:35 +0300

    Add test to check if the deposit() function is run

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -6,7 +6,8 @@ import { Wallet } from "./Wallet";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Wallet", () => {
-  const props = { balance: 20 };
+  const mockDeposit = jest.fn();
+  const props = { balance: 20, deposit: mockDeposit };
   const wallet = shallow(<Wallet {...props} />);
   it("renders properly ", () => {
     expect(wallet).toMatchSnapshot();
@@ -30,6 +31,14 @@ describe("Wallet", () => {
 
     it("updates the local wallet balance in `state` and converts it to a number", () => {
       expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
+
+    describe("and the user wants to make a deposit", () => {
+      beforeEach(() => wallet.find(".btn-deposit").simulate("click"));
+
+      it("dispatches the `deposit()` it receives from local balance", () => {
+        expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
     });
   });
 });
