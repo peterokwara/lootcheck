43c7f02ce1d945eff63857c34bb104aa7cd1c7aa
peterokwara <peter.okwara@outlook.com>
2019-06-27 18:28:47 +0300

    Write test to mock the withdrawal fuction

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -7,7 +7,8 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe("Wallet", () => {
   const mockDeposit = jest.fn();
-  const props = { balance: 20, deposit: mockDeposit };
+  const mockWithdraw = jest.fn();
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw };
   const wallet = shallow(<Wallet {...props} />);
   it("renders properly ", () => {
     expect(wallet).toMatchSnapshot();
@@ -36,8 +37,16 @@ describe("Wallet", () => {
     describe("and the user wants to make a deposit", () => {
       beforeEach(() => wallet.find(".btn-deposit").simulate("click"));
 
-      it("dispatches the `deposit()` it receives from local balance", () => {
+      it("dispatches the `deposit()` it receives from props with local balance", () => {
         expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
+
+    describe("and the user wants to make a withdrawal", () => {
+      beforeEach(() => wallet.find(".btn-withdraw").simulate("click"));
+
+      it("dispatches the `withdraw()` it receives from props with the local balance", () => {
+        expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
       });
     });
   });
