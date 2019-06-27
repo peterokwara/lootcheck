90d982e1b16927de77c4e01682c6f12b13dd765f
peterokwara <peter.okwara@outlook.com>
2019-06-27 12:13:18 +0300

    Add test to display value in wallet

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import Wallet from "./Wallet";
+import { Wallet } from "./Wallet";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("Wallet", () => {
-  const wallet = shallow(<Wallet />);
+  const props = { balance: 20 };
+  const wallet = shallow(<Wallet {...props} />);
   it("renders properly ", () => {
     expect(wallet).toMatchSnapshot();
+  });
+
+  it("displays the balance from props", () => {
+    expect(wallet.find(".balance").text()).toEqual("Wallet balance: 20");
   });
 });
